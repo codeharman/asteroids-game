fcb0b3c092d89edc2ca2599a7e2767bba4fd1a6e
ThePrimeagen <161106030+codeharman@users.noreply.github.com>
2026-06-20 22:33:53 +0530

    addded player file

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import pygame 
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y =  SCREEN_HEIGHT / 2
+    player = Player(x,y)
+    dt = 0.0
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
 
-clock = pygame.time.Clock()
-dt = 0.0
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-while True:
-    log_state()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    screen.fill("black")
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
