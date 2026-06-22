55b7846aac81cc312811d612f858ce295d81055c
ThePrimeagen <161106030+codeharman@users.noreply.github.com>
2026-06-22 22:56:58 +0530

    groups added to the main fiel

--- main.py
+++ main.py
@@ -10,21 +10,25 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     x = SCREEN_WIDTH / 2
     y =  SCREEN_HEIGHT / 2
     player = Player(x,y)
     dt = 0.0
-
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
-
-        player.update(dt)
+        for i in drawable:
+            i.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
